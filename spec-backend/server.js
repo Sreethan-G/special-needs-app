700990bdf5a03dadcc7133fdc20a2635e9ac072f
Sreethan Gangavarapu <sreethan8809@gmail.com>
2025-12-27 12:23:00 -0500

    Adding health check for render

--- spec-backend/server.js
+++ spec-backend/server.js
@@ -43,6 +43,10 @@ app.use(cors({
 app.use(express.json());
 app.use(cookieParser());
 
+app.get("/health", (req, res) => {
+  res.status(200).send("OK");
+});
+
 /* ✅ API routes */
 app.use("/api/resources", resourceRoutes);
 app.use("/api/users", userRoutes);
